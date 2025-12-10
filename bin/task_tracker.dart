0197a676e6c122f7c9cf9ba8128f79c4aa0559f5
Zarno_Bush <zarnobush@gmail.com>
2025-12-10 15:56:32 +0200

    Initial commit

---
+++ bin/task_tracker.dart
@@ -0,0 +1,14 @@
+// import 'package:task_tracker/task_tracker.dart' as task_tracker;
+import 'dart:io';
+
+void main(List<String> arguments) {
+  // print('Hello world: ${task_tracker.calculate()}!');
+
+  //For loop
+  // For In Loop
+  //While Loop
+
+  print("Enter Your Name:");
+  String? name = stdin.readLineSync();
+  print("Hello $name");
+}
---
+++ test/task_tracker_test.dart
@@ -0,0 +1,8 @@
+import 'package:task_tracker/task_tracker.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
