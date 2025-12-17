5ef5c22614ea1ffa288ac57d1af7c71b74499e13
Zarno_Bush <zarnobush@gmail.com>
2025-12-17 11:45:51 +0200

    Json storing

--- bin/task_tracker.dart
+++ bin/task_tracker.dart
@@ -1,14 +1,46 @@
-// import 'package:task_tracker/task_tracker.dart' as task_tracker;
 import 'dart:io';
+import 'dart:convert';
 
 void main(List<String> arguments) {
-  // print('Hello world: ${task_tracker.calculate()}!');
+  print("Welcome to Task Tracker!\nTo Get Started");
 
-  //For loop
-  // For In Loop
-  //While Loop
-
-  print("Enter Your Name:");
+  print('Enter Your Name: ');
   String? name = stdin.readLineSync();
-  print("Hello $name");
+
+  print('Enter Your Email: ');
+  String? email = stdin.readLineSync();
+
+  print('Enter Your Password: ');
+  String? password = stdin.readLineSync();
+
+  Map<String, dynamic> creds = {
+    'name': name,
+    'email': email,
+    'password': password,
+    'created_at': DateTime.now().toIso8601String(),
+  };
+
+  saveToJson(creds);
+
+  print('\nUser saved successfully ✅');
+}
+
+void saveToJson(Map<String, dynamic> data) {
+  final file = File('users.json');
+
+  List users = [];
+
+  // If file exists, read existing data
+  if (file.existsSync()) {
+    final content = file.readAsStringSync();
+    if (content.isNotEmpty) {
+      users = jsonDecode(content);
+    }
+  }
+
+  // Add new user
+  users.add(data);
+
+  // Save back to file (pretty format)
+  file.writeAsStringSync(JsonEncoder.withIndent('  ').convert(users));
 }
---
+++ lib/user.dart
@@ -0,0 +1,7 @@
+class User {
+  String? name;
+  int? age;
+  bool? isActive;
+
+  User({this.name, this.age, this.isActive});
+}
