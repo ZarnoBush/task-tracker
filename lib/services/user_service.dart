eebc8c7595ad58ef7fedb146724aa701d68cc3a2
Zarno_Bush <zarnobush@gmail.com>
2026-01-07 15:16:10 +0200

    Updating user service class

--- lib/services/user_service.dart
+++ lib/services/user_service.dart
@@ -10,6 +10,20 @@ class UserService {
     await _storage.saveUser(user);
   }
 
+  Future<User?> login(String email, String password) async {
+    final user = await _storage.findByEmail(email);
+
+    if (user == null) {
+      return null;
+    }
+
+    if (user.password != password) {
+      return null;
+    }
+
+    return user;
+  }
+
   Future<List<User>> getAllUsers() async {
     return await _storage.getUsers();
   }
