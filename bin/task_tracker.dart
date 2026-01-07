dfd98bd314e75a284f831d774310e278a048ac12
Zarno_Bush <zarnobush@gmail.com>
2026-01-07 14:25:26 +0200

    Updating the task tracker class

--- bin/task_tracker.dart
+++ bin/task_tracker.dart
@@ -1,53 +1,31 @@
+import 'dart:html_common';
 import 'dart:io';
 import 'dart:convert';
 import 'package:task_tracker/models/user.dart';
+import 'package:task_tracker/services/user_service.dart';
 import 'package:task_tracker/services/user_storage.dart';
 import 'package:task_tracker/services/validators.dart';
+import 'package:task_tracker/utils/input_helper.dart';
 
 void main(List<String> arguments) async {
   print("Welcome to Task Tracker!\nTo Get Started");
 
-  final userStorage = UserStorage();
+  final userStorage = UserService(UserStorage());
 
-  stdout.write("Enter your name: ");
-  String? name = stdin.readLineSync();
-  final nameError = Validators.validateName(name);
+  final name = InputHelper.read(
+    label: 'Enter Name: ',
+    validate: Validators.validateName,
+  );
+  final email = InputHelper.read(
+    label: 'Enter Email: ',
+    validate: Validators.validateEmail,
+  );
+  final password = InputHelper.read(
+    label: 'Enter Password: ',
+    validate: Validators.validatePassword,
+  );
 
-  if (nameError != null) {
-    print('Error: $nameError');
-    return;
-  }
-  stdout.write("Enter your email: ");
-  String? email = stdin.readLineSync();
-  final emailError = Validators.validateEmail(email);
-
-  if (emailError != null) {
-    print('Error: $emailError');
-    return;
-  }
-
-  stdout.write("Enter your password: ");
-  String? password = stdin.readLineSync();
-  final passwordError = Validators.validatePassword(password);
-
-  if (passwordError != null) {
-    print('Error: $passwordError');
-    return;
-  }
-
-  User newUser = User(name: name, email: email, password: password);
-
-  await userStorage.saveUser(newUser);
-
-  print('\nUser Registered Successfully!');
-
-  // Read users back
-  List<User> users = await userStorage.getUsers();
-
-  print('\nRegistered Users:');
-  for (var user in users) {
-    print(
-      'Name: ${user.name}, Email: ${user.email}, Created At: ${user.toJson()['created_at']}',
-    );
-  }
+  final user = User(name: name, email: email, password: password);
+  await userStorage.registerUser(user);
+  print('User registered successfully!');
 }
