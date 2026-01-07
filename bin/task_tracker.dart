99d70ed85206c17b22021c69f1c4c8dfefbc763e
Zarno_Bush <zarnobush@gmail.com>
2026-01-07 12:40:08 +0200

    Json storing

--- bin/task_tracker.dart
+++ bin/task_tracker.dart
@@ -1,6 +1,33 @@
 import 'dart:io';
 import 'dart:convert';
+import 'package:task_tracker/models/user.dart';
+import 'package:task_tracker/services/user_storage.dart';
 
-void main(List<String> arguments) {
+void main(List<String> arguments) async {
   print("Welcome to Task Tracker!\nTo Get Started");
+
+  final userStorage = UserStorage();
+
+  stdout.write("Enter your name: ");
+  String? name = stdin.readLineSync();
+  stdout.write("Enter your email: ");
+  String? email = stdin.readLineSync();
+  stdout.write("Enter your password: ");
+  String? password = stdin.readLineSync();
+
+  User newUser = User(name: name, email: email, password: password);
+
+  await userStorage.saveUser(newUser);
+
+  print('\nUser Registered Successfully!');
+
+  // Read users back
+  List<User> users = await userStorage.getUsers();
+
+  print('\nRegistered Users:');
+  for (var user in users) {
+    print(
+      'Name: ${user.name}, Email: ${user.email}, Created At: ${user.toJson()['created_at']}',
+    );
+  }
 }
